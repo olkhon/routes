9ec5fc469095901abb48041ee5a120c547401bf3
olkhon <alexander.stapel@gmail.com>
2021-01-12 12:29:24 +0100

    implement basic routing for users and about in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import UserDetails from './views/UserDetails';
+import About from './views/About';
 
 function App() {
   return (
     <div className="App">
-     <div>test</div>
+     <h1>Test</h1>
+     <Switch>
+       <Route path="/users">
+         <UserDetails />
+       </Route>
+       <Route path="/about">
+         <About />
+       </Route>
+       <Route path=""></Route>
+     </Switch>
     </div>
   );
 }
